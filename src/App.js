545fabebf8d8880d1311d7827e3921ca5701a0ef
abaidmalik243 <abaidmalik243@gmail.com>
2019-05-20 13:19:43 +0500

    Done, 2nd assignment => calender,rich text box, expand btn toggle, save btn  and check rich text box content and checkbox for show All

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import CKEditor from "react-ckeditor-component";
+// import moment from 'moment';
+import $ from 'jquery';
 // import './App.css';
 
 
@@ -9,12 +14,17 @@ function searchingFor(searchText) {
 }
 
 const heading = { textAlign: 'center', paddingTop: '20px', paddingBottom: '100px' }
+const tableHeadingClr = { color: 'blueviolet' }
+const calendarIcon = { marginLeft: '5px', fontSize: '20px', }
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       searchText: '',
+      news_date: new Date(),
+      diplayTable: true,
+      content: 'content',
       data: [
         {
           date: '20-05-2019',
@@ -60,56 +70,154 @@ class App extends React.Component {
         }
       ]
     }
+    this.updateContent = this.updateContent.bind(this);
+  }
+
+  open_AdTimes_calender = () => {
+    $('#ad_times_date_calender').click();
   }
 
   searchHandler = () => {
     this.setState({ searchText: this.refs.searchText.value });
   }
 
+  showAll = () => {
+    this.setState({ searchText: '' })
+  }
+
+  onChangedate = date => {
+    this.setState({ news_date: date })
+  }
+
+  tableToggle = (e) => {
+    e.preventDefault();
+    this.setState({ diplayTable: !this.state.diplayTable })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Data is: ', this.state.content)
+  }
+  // text editor
+
+  updateContent(newContent) {
+    this.setState({
+      content: newContent
+    })
+  }
+
+  onChange = (evt) => {
+    console.log("onChange fired with event info: ", evt);
+    var newContent = evt.editor.getData();
+    this.setState({
+      content: newContent
+    })
+  }
+
+  onBlur(evt) {
+    console.log("onBlur event called with event info: ", evt);
+  }
+
+  afterPaste(evt) {
+    console.log("afterPaste event called with event info: ", evt);
+  }
 
   render() {
     const { data } = this.state;
     return (
       <div>
-        <h2 style={heading}>Assignment # 01</h2>
+        <h2 style={heading}>Assignment # 01, 02</h2>
+
+        <form onSubmit={this.handleSubmit}>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <label>NEWS DATE: </label>
+                <DatePicker className="form-control"
+                  selected={this.state.news_date}
+                  onChange={this.onChangedate}
+                  id="ad_times_date_calender"
+                />
+                <i className="fa fa-calendar" style={calendarIcon} aria-hidden="true" onClick={this.open_AdTimes_calender}></i>
+
+                <br />
+                <br />
+                {this.state.diplayTable ?
+                  <button onClick={this.tableToggle}>Post News</button>
+                  :
+                  <button onClick={this.tableToggle}>Expand News</button>
+                }
+              </div>
+              <div className="col-md-8">
+                <CKEditor
+                  activeClass="p10"
+                  content={this.state.content}
+                  events={{
+                    "blur": this.onBlur,
+                    "afterPaste": this.afterPaste,
+                    "change": this.onChange
+                  }}
+                />
+                <br />
+                <input type="submit" value="SAVE" style={{ float: 'right' }} />
+              </div>
+            </div>
+          </div>
+
+
+          <br />
+          <br />
+        </form>
 
         <div className="table-responsive">
           <table className="table" style={{ width: '100%' }}>
-            <thead>
+            <thead style={tableHeadingClr}>
               <tr>
-                <th>Date</th>
-                <th>Type</th>
-                <th>classification</th>
-                <th>Filter
-                  <input type="text" placeholder="Search..." ref="searchText" name="searchText" />
-                  <button onClick={this.searchHandler} >Go</button>
-                  <button onClick={() => {
-                    this.setState({ searchText: '' })
-                  }}>All</button>
+                <th>DATE</th>
+                <th>TYPE</th>
+                <th>CLASSIFICATION</th>
+                <th>
+                  <span>RECORD FOUND: {data.length} </span>
+                  <span style={{ color: 'black', marginLeft: '20px' }}>
+                    <input type="checkbox" onClick={this.showAll} /> SHOW ALL
+                  </span>
+
+                  <div style={{ float: 'right' }}>
+                    <span>Filter </span>
+                    <input type="text" placeholder="Search..." ref="searchText" name="searchText" />
+                    <button onClick={this.searchHandler} >Go</button>
+                    <button onClick={this.showAll}>All</button>
+                  </div>
                 </th>
                 <th>Action</th>
               </tr>
             </thead>
-            <tbody>
-              {data.length > 0 ?
-                data.filter(searchingFor(this.state.searchText)).map((record, index) => {
+            {this.state.diplayTable ?
 
-                  return (
-                    <tr key={index}>
-                      <td>{record.date}</td>
-                      <td>{record.type}</td>
-                      <td>{record.classification}</td>
-                      <td>{record.description}</td>
-                      <td>
-                        <button>Delete</button>
-                      </td>
-                    </tr>
-                  )
+              <tbody>
+                {data.length > 0 ?
+                  data.filter(searchingFor(this.state.searchText)).map((record, index) => {
 
-                })
-                : <tr><td colSpan="4">No Found</td></tr>
-              }
-            </tbody>
+                    return (
+                      <tr key={index}>
+                        <td>{record.date}</td>
+                        <td>{record.type}</td>
+                        <td>{record.classification}</td>
+                        <td>{record.description}</td>
+                        <td>
+                          <button>Delete</button>
+                        </td>
+                      </tr>
+                    )
+
+                  })
+                  : <tr><td colSpan="4">No Found</td></tr>
+                }
+              </tbody>
+              :
+              <tbody></tbody>
+            }
           </table>
         </div>
       </div>
